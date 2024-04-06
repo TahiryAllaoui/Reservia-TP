9695ea71619f32deb9381957aa8eb6eee612a64d
TahiryAllaoui <tallaoui31@gmail.com>
2024-04-06 20:32:29 +0300

    Search filter implemanted

---
+++ front/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <title>Reservia</title>
+</head>
+
+<body>
+    <?php require('./components/navbar.php'); ?>
+</body>
+
+</html>
