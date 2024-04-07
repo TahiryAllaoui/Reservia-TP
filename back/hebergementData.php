12fc47cf7cf93d34fd71ad13cf9c68631f11e9b6
TahiryAllaoui <tallaoui31@gmail.com>
2024-04-07 08:00:52 +0300

    Main and side hebergement done

--- back/hebergementData.php
+++ back/hebergementData.php
@@ -5,24 +5,26 @@ class Hotel
    public $price;
    public $note;
    public $image;
+   public $isPopular;
 
    public function
-   __construct($name, $price, $note, $image)
+   __construct($name, $price, $note, $image, $isPopular)
    {
       $this->name = $name;
       $this->price = $price;
       $this->note = $note;
       $this->image = $image;
+      $this->isPopular = $isPopular;
    }
 }
 
 $hotels = array();
-$hotels[] = new Hotel('Auberge la Cannebiere', 25, 4, 'marcus-loke-WQJvWU_HZFo-unsplash.jpg');
-$hotels[] = new Hotel('Hotel du port', 52, 5, 'fred-kleber-gTbaxaVLvsg-unsplash.jpg');
-$hotels[] = new Hotel('Hotel Les mouettes', 76, 4, 'reisetopia-B8WIgxA_PFU-unsplash.jpg');
-$hotels[] = new Hotel('Hotel de la mer', 46, 3, 'annie-spratt-Eg1qcIitAuA-unsplash.jpg');
-$hotels[] = new Hotel('Auberge Le Panier', 23, 4, 'nicate-lee-kT-ZyaiwBe0-unsplash.jpg');
-$hotels[] = new Hotel('Hotel chez Amina', 96, 5, 'febrian-zakaria-M6S1WvfW68A-unsplash.jpg');
-$hotels[] = new Hotel('Hotel Le soleil du matin', 128, 5, 'emile-guillemot-Bj_rcSC5XfE-unsplash.jpg');
-$hotels[] = new Hotel("Au coeur de l'eau Chambres d'hotel", 71, 4, 'aw-creative-VGs8z60yT2c-unsplash.jpg');
-$hotels[] = new Hotel('Hotel Tout bleu et Blanc', 68, 4, 'febrian-zakaria-sjvU0THccQA-unsplash.jpg');
+$hotels[] = new Hotel('Auberge la Cannebiere', 25, 4, 'marcus-loke-WQJvWU_HZFo-unsplash.jpg', false);
+$hotels[] = new Hotel('Hotel du port', 52, 5, 'fred-kleber-gTbaxaVLvsg-unsplash.jpg', false);
+$hotels[] = new Hotel('Hotel Les mouettes', 76, 4, 'reisetopia-B8WIgxA_PFU-unsplash.jpg', false);
+$hotels[] = new Hotel('Hotel de la mer', 46, 3, 'annie-spratt-Eg1qcIitAuA-unsplash.jpg', false);
+$hotels[] = new Hotel('Auberge Le Panier', 23, 4, 'nicate-lee-kT-ZyaiwBe0-unsplash.jpg', false);
+$hotels[] = new Hotel('Hotel chez Amina', 96, 5, 'febrian-zakaria-M6S1WvfW68A-unsplash.jpg', false);
+$hotels[] = new Hotel('Hotel Le soleil du matin', 128, 5, 'emile-guillemot-Bj_rcSC5XfE-unsplash.jpg', true);
+$hotels[] = new Hotel("Au coeur de l'eau Chambres d'hotel", 71, 4, 'aw-creative-VGs8z60yT2c-unsplash.jpg', true);
+$hotels[] = new Hotel('Hotel Tout bleu et Blanc', 68, 4, 'febrian-zakaria-sjvU0THccQA-unsplash.jpg', true);
---
+++ front/components/hebergement.php
@@ -0,0 +1,66 @@
+<?php require('../../back/hebergementData.php') ?>
+
+<div id="hebergement">
+  <div id="main-article">
+    <h3>Hebergements a Marseille</h3>
+    <div class="hotels">
+      <?php foreach ($hotels as $hotel) : ?>
+        <?php if (!($hotel->isPopular)) { ?>
+          <div class="card">
+            <img src=<?php echo "../assets/images/hebergements/medium/" . $hotel->image ?> alt=<?php echo $hotel->name ?>>
+            <h4><?php echo $hotel->name ?></h4>
+            <p>Nuit a partir de <?php echo $hotel->price . "$" ?></p>
+            <div class="note">
+              <?php for ($i = 1; $i <= 5; $i++) { ?>
+                <?php if ($i <= $hotel->note) { ?>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
+                    <path fill="#0065FC" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                  </svg>
+                <?php } else { ?>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
+                    <path fill="#F2F2F2" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                  </svg>
+                <?php } ?>
+              <?php } ?>
+            </div>
+          </div>
+        <?php } ?>
+      <?php endforeach ?>
+    </div>
+    <h4>Afficher plus</h4>
+  </div>
+  <div id="side-article">
+    <h3>
+      <span>Les plus populaires</span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
+        <path d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64V400c0 44.2 35.8 80 80 80H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H80c-8.8 0-16-7.2-16-16V64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z" />
+      </svg>
+    </h3>
+    <div class="popular-hotels">
+      <?php foreach ($hotels as $hotel) : ?>
+        <?php if ($hotel->isPopular) { ?>
+          <div class="side-card">
+            <img src=<?php echo "../assets/images/hebergements/small/" . $hotel->image ?> alt=<?php echo $hotel->name ?>>
+            <div class="side-note">
+              <h4><?php echo $hotel->name ?></h4>
+              <p>Nuit a partir de <?php echo $hotel->price . "$" ?></p>
+              <div class="note">
+                <?php for ($i = 1; $i <= 5; $i++) { ?>
+                  <?php if ($i <= $hotel->note) { ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
+                      <path fill="#0065FC" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                    </svg>
+                  <?php } else { ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
+                      <path fill="#F2F2F2" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                    </svg>
+                  <?php } ?>
+                <?php } ?>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+      <?php endforeach ?>
+    </div>
+  </div>
+</div>
