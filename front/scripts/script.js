ca2e22d044d30f46e582b953b5ac790faaedb61f
TahiryAllaoui <tallaoui31@gmail.com>
2024-04-09 11:00:23 +0300

    Search done on mobile

--- front/scripts/script.js
+++ front/scripts/script.js
@@ -5,12 +5,12 @@ aHeberge.addEventListener("click", () => {
   heberge.scrollIntoView({ behavior: "smooth" });
 });
 
-// For anchor mobile
+// For Menu on mobile
 let nav = document.querySelector("nav");
 let anchor = document.querySelector(".anchor");
 let closeMenu = document.querySelector(".anchor h4");
 let body = document.querySelector("body");
-if (body.clientWidth < 428) {
+if (body.clientWidth < 435) {
   let svgs = document.createElement("div");
   svgs.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="width: 2rem; padding: 0 1rem 0 0">
     <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
