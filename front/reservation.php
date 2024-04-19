4b8c1f19b0a2368593eee0579dd3ad44839c5579
TahiryAllaoui <tallaoui31@gmail.com>
2024-04-19 16:24:58 +0300

    Reservation animation done

--- front/reservation.php
+++ front/reservation.php
@@ -24,21 +24,23 @@ require('../back/hebergementData.php') ?>
     <div id="reservation">
         <img src=<?php echo "./assets/images/hebergements/large/" . $hotels[$index]->image ?> alt=<?php echo $hotels[$index]->name ?>>
         <div class="summary">
-            <h2><?php echo $hotels[$index]->name ?></h2>
-            <div class="note">
-                <?php for ($i = 1; $i <= 5; $i++) { ?>
-                    <?php if ($i <= $hotels[$index]->note) { ?>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                            <path fill="#0065FC" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-                        </svg>
-                    <?php } else { ?>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                            <path fill="#d7d7d7" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-                        </svg>
+            <div class="info">
+                <h2><?php echo $hotels[$index]->name ?></h2>
+                <div class="note">
+                    <?php for ($i = 1; $i <= 5; $i++) { ?>
+                        <?php if ($i <= $hotels[$index]->note) { ?>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path fill="#0065FC" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                            </svg>
+                        <?php } else { ?>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path fill="#d7d7d7" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                            </svg>
+                        <?php } ?>
                     <?php } ?>
-                <?php } ?>
+                </div>
+                <p>Nuit a partir de <strong><?php echo $hotels[$index]->price . "$" ?></strong></p>
             </div>
-            <p>Nuit a partir de <strong><?php echo $hotels[$index]->price . "$" ?></strong></p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil similique commodi dolorum minima fugit nam officia veritatis ducimus libero. Reprehenderit, saepe architecto debitis eos sit harum officia doloribus autem, eum voluptatum esse voluptas? Quasi cumque aliquid enim aliquam reiciendis nisi omnis fuga minima dignissimos, optio voluptas laudantium deserunt quis repellendus.</p>
             <div class="action">
                 <div class="back">Retour</div>
